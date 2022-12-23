6d43274a5e9f168613d17142db6f02d22cb10d01
Joonas Mattinen <112629116+JoonasMattinen@users.noreply.github.com>
2022-12-23 13:03:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("firstImg").onclick = function(){
+    window.location = "./pages/tietoa.html";
+}
+document.getElementById("secondImg").onclick = function(){
+    window.location = "./pages/draivi.html";
+}
+document.getElementById("thirdImg").onclick = function(){
+    window.location = "./pages/puttaus.html";
+}
